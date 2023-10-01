c7d72cdc62715d410908fd61c8b6cb1cefae22be
fadi <fadi.m.marouf@gmail.com>
2023-10-01 08:46:15 +0200

    color flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "yellow"];
+const btn= document.getElementById("btn")
+const bg=document.getElementById("bg")
+const color=document.querySelector(".color")
+
+let colorNr=0
+const flip=()=>{
+bg.style.background = colors[colorNr];
+color.style.background = colors[colorNr];
+color.innerHTML = colors[colorNr];
+colorNr < colors.length-1? (colorNr++, console.log(colorNr)): colorNr=0
+}
