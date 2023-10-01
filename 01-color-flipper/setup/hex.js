69490660cf34ce5001667a40b2ccf2019f9d322d
fadi <fadi.m.marouf@gmail.com>
2023-10-01 13:09:06 +0200

    add hex js

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn= document.getElementById("btn")
+const color=document.querySelector(".color")
+const cont= document.querySelector("main")
+btn.style.background="red"
+btn.addEventListener
+    ("click",
+    () => {
+        
+        const randomNum = generateNum();
+        color.innerHTML=randomNum
+        cont.style.background=randomNum
+    });
+
+const generateNum=()=>{
+    
+    let color="#"
+    for(i=0; i< 6;i++){
+        console.log(color)
+color += hex[Math.floor(Math.random() * hex.length)];
+
+    }
+   return color; 
+}
