0976111d7c91f29004e5ddc0e80a70aa1df7e103
fadi <fadi.m.marouf@gmail.com>
2023-10-01 21:18:34 +0200

    e.currentTarget.classList.contains

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,26 +1,39 @@
-const dec=document.querySelector(".dec")
-const inc=document.querySelector(".inc")
-const res=document.querySelector(".res")
+const btns=document.querySelectorAll(".bttn")
 const count=document.querySelector(".counter")
-
+console.log(btns)
 let num=0
-
-inc.addEventListener("click",()=>{
-    num++
-    count.innerHTML = num;
-    count.style.color = num > 0? "red": num<0? "green" : "black"
+btns.forEach(btn => {
+    btn.addEventListener("click",(e) => {
+       e.currentTarget.classList.contains("inc")
+           ? num++
+           : e.currentTarget.classList.contains("dec")
+           ? num--
+           : num = 0;
+          count.innerHTML = num;
+          count.style.color = num > 0 ? "red" : num < 0 ? "green" : "black";  
+    }
+        
+    );
     
 }
 
-)
-dec.addEventListener("click",()=>{
-    num--
-count.innerHTML = num;
-count.style.color = num > 0 ? "red" : num < 0 ? "green" : "black";
+);
+// inc.addEventListener("click",()=>{
+//     num++
+//     count.innerHTML = num;
+//     count.style.color = num > 0? "red": num<0? "green" : "black"
     
-})
-res.addEventListener("click",()=>{
-num=0
-count.innerHTML = num;
-count.style.color = num > 0 ? "red" : num < 0 ? "green" : "black";
-})
+// }
+
+// )
+// dec.addEventListener("click",()=>{
+//     num--
+// count.innerHTML = num;
+// count.style.color = num > 0 ? "red" : num < 0 ? "green" : "black";
+    
+// })
+// res.addEventListener("click",()=>{
+// num=0
+// count.innerHTML = num;
+// count.style.color = num > 0 ? "red" : num < 0 ? "green" : "black";
+// })
