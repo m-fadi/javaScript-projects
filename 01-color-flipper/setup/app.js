69ca2ade440eff88b8268411b00cc3d6b73272fc
fadi <fadi.m.marouf@gmail.com>
2023-10-01 10:52:00 +0200

    style

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,12 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "yellow"];
 const btn= document.getElementById("btn")
 const bg=document.getElementById("bg")
-const color=document.querySelector(".color")
+const col=document.querySelector(".col")
 
 let colorNr=0
 const flip=()=>{
 bg.style.background = colors[colorNr];
-color.style.background = colors[colorNr];
-color.innerHTML = colors[colorNr];
+
+col.innerHTML = colors[colorNr];
 colorNr < colors.length-1? (colorNr++, console.log(colorNr)): colorNr=0
 }
