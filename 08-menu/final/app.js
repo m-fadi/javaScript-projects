82cbb4e0c35c167089b0ac7bfb9d4f7f32993848
fadi <fadi.m.marouf@gmail.com>
2023-10-05 19:58:17 +0200

    add

--- 08-menu/final/app.js
+++ 08-menu/final/app.js
@@ -85,11 +85,11 @@ const sectionCenter = document.querySelector(".section-center");
 const btnContainer = document.querySelector(".btn-container");
 // display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
-  diplayMenuItems(menu);
+  displayMenuItems(menu);
   displayMenuButtons();
 });
 
-function diplayMenuItems(menuItems) {
+function displayMenuItems(menuItems) {
   let displayMenu = menuItems.map(function (item) {
     // console.log(item);
 
