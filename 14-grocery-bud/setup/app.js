ae07b7626f8c8929d02055f9fa59353dda925832
fadi <fadi.m.marouf@gmail.com>
2023-10-06 09:43:12 +0200

    select items

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -9,3 +9,19 @@
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
+const input = document.querySelector("#grocery").value;
+const groceryItems= document.querySelector(".grocery-item");
+const editBtn= document.querySelector(".edit-btn");
+const deleteBtn= document.querySelector(".delete-btn");
+const clearBtn= document.querySelector(".clear-btn");
+const submitBtn= document.querySelector(".submit-btn");
+// ****** EVENT LISTENERS **********
+const inputHandler = (e) => {
+    '//e.preventDefault(); 
+    const input = e.target.value;
+    groceryItems.innerHTML = input;
+}
+submitBtn.addEventListener("click", (e) => {
+    
+    inputHandler(e)
+})
