1fd42306e9f78f38efefa215c029f74b3de6cffd
fadi <fadi.m.marouf@gmail.com>
2023-10-08 22:34:33 +0200

    delete

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -9,19 +9,49 @@
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
-const input = document.querySelector("#grocery").value;
-const groceryItems= document.querySelector(".grocery-item");
+const input = document.querySelector("#grocery");
+const groceryList= document.querySelector(".grocery-list");
 const editBtn= document.querySelector(".edit-btn");
-const deleteBtn= document.querySelector(".delete-btn");
+;
 const clearBtn= document.querySelector(".clear-btn");
 const submitBtn= document.querySelector(".submit-btn");
 // ****** EVENT LISTENERS **********
-const inputHandler = (e) => {
-    '//e.preventDefault(); 
-    const input = e.target.value;
-    groceryItems.innerHTML = input;
-}
-submitBtn.addEventListener("click", (e) => {
+
+
+const deleteBtn = document.querySelectorAll(".delete-btn");
+
+clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(deleteBtn);
+    groceryList.innerHTML=""
     
-    inputHandler(e)
-})
+});
+
+// deleteBtn.addEventListener("click", (e) => {
+
+// })
+
+
+const addGroceryItem = (item) => {
+    
+    return `
+                        <p class="title">${item}</p>
+                        <div class="btn-container">
+                            <button class="edit-btn" type="button">
+                                <i class="fas fa-edit"></i>
+                            </button>
+                            <button class="delete-btn" type="button">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </div>
+                    `
+}
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const element = document.createElement("article");
+    element.innerHTML = addGroceryItem(input.value);
+    groceryList.appendChild(element);
+    input.value = "";
+    console.log(deleteBtn);
+});
