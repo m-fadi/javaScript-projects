0429722fc8affb705f287aa82d1df74f07d208ad
fadi <fadi.m.marouf@gmail.com>
2023-10-09 16:29:49 +0200

    edit

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -9,6 +9,7 @@
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
+const form = document.querySelector(".grocery-form");
 const input = document.querySelector("#grocery");
 const groceryList= document.querySelector(".grocery-list");
 const editBtn= document.querySelector(".edit-btn");
@@ -18,40 +19,5 @@ const submitBtn= document.querySelector(".submit-btn");
 // ****** EVENT LISTENERS **********
 
 
-const deleteBtn = document.querySelectorAll(".delete-btn");
-
-clearBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log(deleteBtn);
-    groceryList.innerHTML=""
-    
-});
-
-// deleteBtn.addEventListener("click", (e) => {
-
-// })
 
 
-const addGroceryItem = (item) => {
-    
-    return `
-                        <p class="title">${item}</p>
-                        <div class="btn-container">
-                            <button class="edit-btn" type="button">
-                                <i class="fas fa-edit"></i>
-                            </button>
-                            <button class="delete-btn" type="button">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                    `
-}
-
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const element = document.createElement("article");
-    element.innerHTML = addGroceryItem(input.value);
-    groceryList.appendChild(element);
-    input.value = "";
-    console.log(deleteBtn);
-});
