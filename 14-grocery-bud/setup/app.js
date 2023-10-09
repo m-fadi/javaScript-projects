196676d80a61611e368435dac0263f4ae9dd1d12
fadi <fadi.m.marouf@gmail.com>
2023-10-09 20:55:05 +0200

    new start

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -15,9 +15,39 @@ const groceryList= document.querySelector(".grocery-list");
 const editBtn= document.querySelector(".edit-btn");
 ;
 const clearBtn= document.querySelector(".clear-btn");
-const submitBtn= document.querySelector(".submit-btn");
+
 // ****** EVENT LISTENERS **********
 
 
 
+const addItem = (e) => {
+    e.preventDefault();
+    const value = input.value;
+    const element = document.createElement("article");
+    element.classList.add("grocery-item");
+    console.log(element)
+    element.innerHTML= createItemHtml(value)
+    groceryList.appendChild(element);
+    const deleteBtn = document.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+}
+const deleteItem = (e) => {
+    const element = e.target.parentElement.parentElement;
+    console.log("element", element);
+    groceryList.removeChild(element);
+};
+form.addEventListener("submit", addItem);
 
+
+
+const createItemHtml = (value) => {
+    return `<p class="title">${value}</p>
+                        <div class="btn-container">
+                            <button class="edit-btn" type="button">
+                                <i class="fas fa-edit"></i>
+                            </button>
+                            <button class="delete-btn" type="button">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </div>`;
+};
