9ea238ccf318f8b13d608f09fe0e72392ea48555
fadi <fadi.m.marouf@gmail.com>
2023-10-19 10:56:14 +0200

    change submit to edit

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -11,54 +11,54 @@
 // ****** SETUP ITEMS **********
 const form = document.querySelector(".grocery-form");
 const input = document.querySelector("#grocery");
-const groceryList= document.querySelector(".grocery-list");
-const editBtn= document.querySelector(".edit-btn");
-
-const clearBtn= document.querySelector(".clear-btn");
+const groceryList = document.querySelector(".grocery-list");
+const editBtn = document.querySelector(".edit-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const clearBtn = document.querySelector(".clear-btn");
 
 // ****** EVENT LISTENERS **********
-let editFlag=false
-
+let editFlag = false;
+let editElement = "";
 
 const addItem = (e) => {
-    if(editFlag){
-        console.log(value)
-    }
     e.preventDefault();
+    if (editFlag) {
+        editElement.innerHTML = input.value;
+        input.value = "";
+        submitBtn.textContent = "submit";
+        return;
+    }
+
     const value = input.value;
     let id = Date.now();
     const element = document.createElement("article");
     element.classList.add("grocery-item");
-    element.setAttribute('data-id' , id);
-    
-    localStorage.setItem(id, value)
+    element.setAttribute("data-id", id);
+
+    localStorage.setItem(id, value);
     element.innerHTML = createItemHtml(localStorage.getItem(id));
     groceryList.appendChild(element);
     const deleteBtn = element.querySelector(".delete-btn");
-    input.value=""
+    input.value = "";
     deleteBtn.addEventListener("click", deleteItem);
     const editBtn = element.querySelector(".edit-btn");
     deleteBtn.addEventListener("click", deleteItem);
-    editBtn.addEventListener("click", () => editItem(value));
-}
-const deleteItem = (e) => {
-    console.log("delete")
-    const element = e.target.parentElement.parentElement.parentElement;
-       groceryList.removeChild(element);
+    editBtn.addEventListener("click", editItem);
 };
-const editItem=(value) => {
-    editFlag=true
-
-    console.log("edit",value)
-    
-input.value=value
-
-}
+const deleteItem = (e) => {
+    const element = e.currentTarget.parentElement.parentElement;
+    groceryList.removeChild(element);
+};
+const editItem = (e) => {
+    editFlag = true;
+    editElement = e.currentTarget.parentElement.previousElementSibling;
+    submitBtn.textContent = "Edit";
+    input.value = editElement.innerHTML;
+};
 
 form.addEventListener("submit", addItem);
 
 const createItemHtml = (value) => {
-    
     return `<p class="title">${value}</p>
                         <div class="btn-container">
                             <button class="edit-btn" type="button">
