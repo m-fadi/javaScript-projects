578878dcd50a3dab72c70758388da76e59df9e0f
fadi <fadi.m.marouf@gmail.com>
2023-11-07 21:01:56 +0100

    kj

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,29 @@
+let car = {
+    company: "mazda",
+    model: "1999",
+    drive: function () {
+        console.log("I drive" + " " + this.company + " " + this.model);
+    },
+};
+car.drive();
+
+// In HTML, create a div element and a button.
+
+// When the user clicks the button, set the background color of the div to red.
+// In HTML, create a button and a div element.
+
+// When the user clicks the button, update the textContent of the div element with the amount of times the user has clicked the button.
+
+// Example: "You clicked 12 times"
+
+// We're creating a web shop selling hummus (or chocolate, or eba and egusi soup)!
+
+// Create a number input field in HTML that lets the user choose the amount of hummus:
+
+// Choose the amount of hummus servings:
+// <input type="number" id="amount" min="0" value="0" oninput="amountChanged()">
+// Use the valueAsNumber property of the number input to get the amount that the user selected in your amountChanged function. Output the total price the user has to pay to a div element.
+
+// Extend your webshop to sell two products (e.g. hummus and chocolate).
+
+// Every product has a different price. Update the total price in the div element every time the user changes the amount of hummus and chocolate.
